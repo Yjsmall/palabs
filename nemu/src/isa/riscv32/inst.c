583befe2eb5e6195dca587940733cc946ba69405
Fisher <1186907891@qq.com>
2024-10-19 16:55:45 +0800

    PA2.1 add a type

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -51,8 +51,10 @@ enum {
         *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); \
     } while (0)
 
-#define immJ() do { *imm = SEXT((BITS(i, 31, 31) << 19) | (BITS(i, 30, 21)) | (BITS(i, 20, 20) << 10) | (BITS(i, 19, 12) << 11), 20) << 1; } while(0)
-
+#define immJ()                                                                                                                 \
+    do {                                                                                                                       \
+        *imm = SEXT((BITS(i, 31, 31) << 19) | (BITS(i, 30, 21)) | (BITS(i, 20, 20) << 10) | (BITS(i, 19, 12) << 11), 20) << 1; \
+    } while (0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
     uint32_t i   = s->isa.inst;
