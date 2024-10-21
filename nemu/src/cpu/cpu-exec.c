7acbdba7f7ce097633ee82cc1100bb81ac0a9d33
tracer-ics2024 <tracer@njuics.org>
2024-10-21 17:07:31 +0800

    >  compile NEMU unkonow Fisher Linux fisher 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:07:31 up  3:31,  1 user,  load average: 0.08, 0.11, 0.12

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -80,9 +80,9 @@ static void exec_once(Decode *s, vaddr_t pc) {
 static void execute(uint64_t n) {
     Decode s;
     for (; n > 0; n--) {
-        printf("before: cpu.pc = %08x\n, cpu.snpc = 08x\n, cpu.dnpc = 08x\n", cpu.pc);
+        printf("before: cpu.pc = %08x\n", cpu.pc);
         exec_once(&s, cpu.pc);
-        printf("after: cpu.pc = %08x\n, cpu.snpc = 08x\n, cpu.dnpc = 08x\n", cpu.pc);
+        printf("after: cpu.pc = %08x\n", cpu.pc);
         g_nr_guest_inst++;
         trace_and_difftest(&s, cpu.pc);
         if (nemu_state.state != NEMU_RUNNING) break;
