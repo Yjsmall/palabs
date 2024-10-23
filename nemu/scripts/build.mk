06127210ae8b6ac5d53153fc6b82836a330e532a
tracer-ics2024 <tracer@njuics.org>
2024-10-23 20:37:39 +0800

    >  compile NEMU unkonow Fisher Linux fisher 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:37:39 up  4:32,  1 user,  load average: 0.10, 0.07, 0.09

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -22,11 +22,8 @@ CXX := g++
 endif
 LD := $(CXX)
 INCLUDES = $(addprefix -I, $(INC_PATH))
-# here modify the CFLAGS and LDFLAGS -O2 -> -O0
-CFLAGS  := -O0 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
-CFLAGS += -DBATCH_MODE
-
-LDFLAGS := -O0 $(LDFLAGS)
+CFLAGS  := -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
+LDFLAGS := -O2 $(LDFLAGS)
 
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
