a226dc83cc0f3d7baf41feebeabbfd69ae675aed
tracer-ics2024 <tracer@njuics.org>
2024-10-23 20:18:58 +0800

    >  compile NEMU unkonow Fisher Linux fisher 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:18:58 up  4:13,  1 user,  load average: 0.05, 0.12, 0.16

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -6,6 +6,10 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
+  if (s == NULL) {
+    return 0;
+  }
+
   size_t len = 0;
   while (*s != '\0') {
     len++;
@@ -16,21 +20,24 @@ size_t strlen(const char *s) {
 
 char *strcpy(char *dst, const char *src) {
   char *original_dest = dst;
-  while ((*dst++ = *src++)) {
-  
+  while (*src != '\0') {
+    *dst++ = *src++;
   }
+  *dst = '\0';
   return original_dest;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  char *original_dest = dst;
-  while (n-- && (*dst++ = *src++)) {
+  size_t i = 0;
+  for (i = 0; i < n && src[i] != '\0'; i++) {
+    dst[i] = src[i];
   }
 
-  while (n--) {
-    *dst++ = '\0';
+  for (; i < n; i++) {
+    dst[i] = '\0';
   }
-  return original_dest;
+
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
@@ -39,7 +46,8 @@ char *strcat(char *dst, const char *src) {
     dst++;
   }
 
-  while ((*dst++ = *src)) {
+  while (*src) {
+    *dst++ = *src++;
   }
 
   return original_dest;
@@ -72,42 +80,34 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  unsigned char *p = (unsigned char *)s;
+    unsigned char* p = (unsigned char*)s;
 
-  while (n--) {
-    *p++ = (unsigned char)c;
-  }
-
-  return s;
-}
-
-void *memmove(void *dst, const void *src, size_t n) {
-  unsigned char *d = (unsigned char *)dst;
-  const unsigned char *s = (const unsigned char *)src;
-
-  if (d > s && d < s + n) {
-    d += n;
-    s += n;
-    while (n--) {
-      *(d++) = *(s++);
+    for (size_t i = 0; i < n; i++) {
+        p[i] = (unsigned char)c;
     }
-  } else {
-    while (n--) {
-      *(d++) = *(s++);
-    }
-  }
-
-  return dst;
+    return s;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  unsigned char *d = (unsigned char *)out;
-  const unsigned char *s = (const unsigned char *)in;
+    if (!out || !in)
+        return NULL;
+    // 确保out和in指向的内存地址不重叠
+    if ((char*)out < (char*)in || (char*)out >= (char*)in + n) {
+        // 没有重叠，直接复制
+        for (size_t i = 0; i < n; ++i) {
+            ((char*)out)[i] = ((char*)in)[i];
+        }
+    } else {
+        // 有重叠，从后向前复制以避免数据被覆盖
+        for (size_t i = n; i != 0; --i) {
+            ((char*)out)[i - 1] = ((char*)in)[i - 1];
+        }
+    }
+    return out;
+}
 
-  while (n--) {
-    *d++ = *s++;
-  }
-  return out;
+void *memmove(void *dst, const void *src, size_t n) {
+    return memcpy(dst, src, n);
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
