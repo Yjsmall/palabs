2ec61363dddb244a1b3b78686c8a9cc3fc73c8ac
tracer-ics2024 <tracer@njuics.org>
2024-10-24 20:36:24 +0800

    >  compile NEMU unkonow Fisher Linux fisher 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:36:24 up 12:00,  1 user,  load average: 0.12, 0.09, 0.04

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -43,7 +43,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
         log_write("%s\n", _this->logbuf);
         char *dest = malloc(strlen(_this->logbuf) + 1);
         strcpy(dest, _this->logbuf);
-        add_inst(_this->logbuf);
+        add_inst(dest);
     }
 #endif
     if (g_print_step) {
