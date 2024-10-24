3458f89014e0ab575d85b2edc1b642f75e895cfd
tracer-ics2024 <tracer@njuics.org>
2024-10-24 20:45:50 +0800

    >  compile NEMU unkonow Fisher Linux fisher 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:45:50 up 12:09,  1 user,  load average: 0.07, 0.09, 0.07

--- nemu/include/cpu/tracebuffer.h
+++ nemu/include/cpu/tracebuffer.h
@@ -1,27 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-char * buffer[16];
-int cur = -1;
-int size = 0;
+
+char *buffer[16];
+int   cur  = -1;
+int   size = 0;
 
 void add_inst(char *inst) {
-  if (size >= 16) {
-    free(buffer[cur]);
-  }
-    cur = (cur + 1) % 16;
+    if (size >= 16) {
+        free(buffer[cur]);
+        buffer[cur] = NULL; // Set to NULL after freeing
+    }
+    cur         = (cur + 1) % 16;
     buffer[cur] = inst;
-    size++;
+    if (size < 16) {
+        size++;
+    }
 }
 
 void print_buffer() {
     for (int i = 0; i < 16; i++) {
-        printf("%s\n", buffer[i]);
+        if (buffer[i] != NULL) {
+            printf("%s\n", buffer[i]);
+        }
     }
 }
 
 void destory_buffer() {
     for (int i = 0; i < 16; i++) {
-      free(buffer[i]);
+        if (buffer[i] != NULL) {
+            free(buffer[i]);
+            buffer[i] = NULL; // Set to NULL after freeing
+        }
     }
     cur = -1;
 }
