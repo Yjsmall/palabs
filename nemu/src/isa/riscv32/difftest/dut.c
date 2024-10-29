69dac22cc7f00af875677f9e92586f9a4eaf13ab
tracer-ics2024 <tracer@njuics.org>
2024-10-29 19:55:09 +0800

    >  compile NEMU unkonow Fisher Linux fisher 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:55:09 up  8:50,  5 users,  load average: 0.01, 0.04, 0.06

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,6 +17,30 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
-bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) { return false; }
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+    int  reg_num  = ARRLEN(cpu.gpr);
+    int  i        = 0;
+    bool is_equal = true;
+    if (ref_r->pc != cpu.pc) {
+        printf("ref_pc" FMT_WORD "  dut_pc:" FMT_WORD "\n", ref_r->pc, cpu.pc);
+        is_equal = false;
+    }
+    for (i = 0; i < reg_num; i++) {
+        if (ref_r->gpr[i] != cpu.gpr[i]) {
+            printf("reg: %s\n""ref->gpr:%u  dut->gpr:%u\n", reg_name(i), ref_r->gpr[i], cpu.gpr[i]);
+            is_equal = false;
+        }
+    }
+    if (is_equal == false) {
+        for (i = 0; i < reg_num; i++) {
+            if (ref_r->gpr[i] >= 0x80000000) {
+                printf("ref-%3s         %#x\n", reg_name(i), ref_r->gpr[i]);
+            } else {
+                printf("ref-%3s         %d\n", reg_name(i), ref_r->gpr[i]);
+            }
+        }
+    }
+    return is_equal;
+}
 
 void isa_difftest_attach() {}
